4cc44b88ab25779a7a126bb4e69740dd35182091
TestEng <jyothi.gowda2277@gmail.com>
2026-05-04 11:47:57 +0530

    new task is completed ticet no 1234

---
+++ src/test/java/com/qa/usermanagement/jyothi/Jyothi.java
@@ -0,0 +1,11 @@
+package com.qa.usermanagement.jyothi;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Jyothi {
+	@Test
+public void name() {
+	Reporter.log("Its just a dummy page",true);
+}
+}
