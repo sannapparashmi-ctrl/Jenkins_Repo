c91fcbbf09bbb8a0daf5337d9184cdf371c20515
VaraPrasad <pinakpani777@gmail.com>
2026-05-04 11:36:25 +0530

    Finished with assigned task,ticketId-1

---
+++ src/test/java/varaprasad/Vara.java
@@ -0,0 +1,11 @@
+package varaprasad;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+@Test
+public class Vara {
+	
+	public void gitTest() {
+		Reporter.log("Testing GitHub Flow here",true);	}
+
+}
