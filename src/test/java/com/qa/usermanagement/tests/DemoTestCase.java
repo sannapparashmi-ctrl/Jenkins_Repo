1aad92206cc64607d3ed5cd0b5424b9fadce7e86
TestEngineer <sannappa.rashmi@gmail.com>
2026-05-07 10:52:49 +0530

    adding test case

---
+++ src/test/java/com/qa/usermanagement/tests/DemoTestCase.java
@@ -0,0 +1,13 @@
+package com.qa.usermanagement.tests;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoTestCase {
+@Test
+	public static void demo() {
+		Reporter.log("This is demo test case",true);
+	}
+	
+	
+}
